47070251e39705978f93da1d9d927a00697d548c
David Tesar <david.tesar@microsoft.com>
2017-07-25 15:45:14 -0700

    single example file

--- omslog.go
+++ omslog.go
@@ -1,119 +1,143 @@
-package omslog
+// OMS Data Collector package adapted from 
+// https://github.com/Azure/oms-log-analytics-firehose-nozzle
+package main
 
 import (
-	"fmt"
-	"time"
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
 
-	"github.com/Sirupsen/logrus"
-	"github.com/docker/docker/daemon/logger"
-	"oms-go/oms_data_collector"
+// Required parameters
+var (
+	// Update customerId to your Operations Management Suite workspace ID
+	omscustomerID    = "11a3f44a-8e89-40e4-9a7e-7f145d8b2c68"
+	// For sharedKey, use either the primary or the secondary Connected Sources client authentication key
+	omssharedKey     = "GpR2PSS0pemW/930iEcwgCcN3GjgmF1BCnYhKUmxVgKCWenMRbRl7pghaDMrEpwzOcIkQLan/jvYhGK5qrLixg=="
+	// HTTP timeout for posting events to OMS Log Analytics
+	omsPostTimeout   = 5 * time.Second
 )
 
 const (
-	name = "omslog"
-
-	// Options
-	workspaceKey = "workspaceId"
-	sharedKey = "sharedKey"
-	timeout = 30 * time.Second
-
-	// Errors
-	errOptRequired = "must specify a value for log opt '%s'"
+	method      = "POST"
+	contentType = "application/json"
+	resource    = "/api/logs"
 )
 
-type omsLogger struct {
-	containerID string
-	containerName string
-	imageID string
-	imageName string
-	client oms_data_collector.OmsLogClient
+type omsMessage struct {
+	SourceSystem   string `json:"sourceSystem,omitempty"`
+	ContainerID    string `json:"containerId"`
+	ContainerName  string `json:"containerName"`
+	TimeGenerated  string  `json:"timeGenerated"`
+	LogEntry       string `json:"logEntry"`
 }
 
-type omsMessage struct {
-	ContainerID string `json:"containerId"`
-	ContainerName string `json:"containerName"`
-	ImageID string `json:"imageId"`
-	ImageName string `json:"imageName"`
-	Timestamp int64 `json:"timestamp"`
-	Source string `json:"source"`
-	Line string `json:"line"`
+type OmsLogClient interface {
+	PostData(*[]byte, string) error
+}
+
+// OmsLogClient posts messages to OMS
+type omslogclient struct {
+	customerID      string
+	sharedKey       string
+	url             string
+	httpPostTimeout time.Duration
+	client			*http.Client
 }
 
 func init() {
-	if err := logger.RegisterLogDriver(name, New); err != nil {
-		logrus.Fatal(err)
-	}
-	if err := logger.RegisterLogOptValidator(name, ValidateLogOpt); err != nil {
-		logrus.Fatal(err)
+	http.DefaultClient.Timeout = time.Second * 30
+}
+
+// New instance of the OmsLogClient
+func NewOmsLogClient(customerID string, sharedKey string, postTimeout time.Duration ) OmsLogClient {
+	return &omslogclient{
+		customerID:      customerID,
+		sharedKey:       sharedKey,
+		url:             "https://" + customerID + ".ods.opinsights.azure.com" + resource + "?api-version=2016-04-01",
+		httpPostTimeout: postTimeout,
+		client: 		 &http.Client{ Timeout: postTimeout	},
 	}
 }
 
-// ValidateLogOpt looks for workspaceKey and sharedKey
-func ValidateLogOpt(cfg map[string]string) error {
-	for key := range cfg {
-		switch key {
-			case workspaceKey:
-			case sharedKey:
-			default:
-				return fmt.Errorf("unknown log opt '%s' for %s log driver", key, name)
-		}
+// PostData posts message to OMS
+func (c *omslogclient) PostData(msg *[]byte, logType string) error {
+	// Headers
+	contentLength := len(*msg)
+	rfc1123date := time.Now().UTC().Format(time.RFC1123)
+	//TODO: rfc1123 date should have UTC offset
+	rfc1123date = strings.Replace(rfc1123date, "UTC", "GMT", 1)
+	//Signature
+	signature, err := c.buildSignature(rfc1123date, contentLength, method, contentType, resource)
+	if err != nil {
+		return err
 	}
-
-	if cfg[workspaceKey] == "" {
-		return fmt.Errorf(errOptRequired, workspaceKey)
+	// Create request
+	req, err := http.NewRequest("POST", c.url, bytes.NewBuffer(*msg))
+	if err != nil {
+		return err
 	}
+	req.Header.Set("Authorization", signature)
+	req.Header.Set("Log-Type", logType)
+	//TODO: headers should be case insentitive
+	//req.Header.Set("x-ms-date", rfc1123date)
+	req.Header["x-ms-date"] = []string{rfc1123date}
+	req.Header.Set("Content-Type", "application/json")
 
-	if cfg[sharedKey] == "" {
-		return fmt.Errorf(errOptRequired, sharedKey)
-	}
-
-	return nil
-}
-
-// New creates an omslog using configuration options passed in via the context.
-func New(info logger.Info) (logger.Logger, error) {
-	workspaceID := info.Config[workspaceKey]
-	sharedKey := info.Config[sharedKey]
-
-	l := &omsLogger{
-		containerID: info.ContainerID,
-		containerName: info.ContainerName,
-		imageID: info.ContainerImageID,
-		imageName: info.ContainerImageName,
-		client: oms_data_collector.NewOmsLogClient(workspaceID, sharedKey, timeout),
-	}
-	
-	return l, nil
-}
-
-func (l *omsLogger) Log(message *logger.Message) error {
-	msg := &omsMessage {
-		ContainerID: l.containerID,
-		ContainerName: l.containerName,
-		ImageID: l.imageID,
-		ImageName: l.imageName,
-		Timestamp: message.Timestamp.UnixNano() / int64(time.Millisecond),
-		Source: message.Source,
-		Line: string(message.Line),
-	}
-
-	buffer, err := json.Marshal(msg)
+	resp, err := c.client.Do(req)
 	if err != nil {
 		return err
 	}
 
-	if err := l.client.PostData(&buffer, "Line"); err != nil {
-		return err
+	defer resp.Body.Close()
+	if resp.StatusCode >= 300 || resp.StatusCode < 200 {
+		return fmt.Errorf("Post Error. HTTP response code:%d message:%s", resp.StatusCode, resp.Status)
 	}
-
+	
+	fmt.Printf("API success code: %d", resp.StatusCode)
 	return nil
 }
 
-func (l *omsLogger) Name() string {
-	return name
+func (c *omslogclient) buildSignature(date string, contentLength int, method string, contentType string, resource string) (string, error) {
+	xHeaders := "x-ms-date:" + date
+	stringToHash := method + "\n" + strconv.Itoa(contentLength) + "\n" + contentType + "\n" + xHeaders + "\n" + resource
+	bytesToHash := []byte(stringToHash)
+	keyBytes, err := base64.StdEncoding.DecodeString(c.sharedKey)
+	if err != nil {
+		return "", err
+	}
+	hasher := hmac.New(sha256.New, keyBytes)
+	hasher.Write(bytesToHash)
+	encodedHash := base64.StdEncoding.EncodeToString(hasher.Sum(nil))
+	authorization := fmt.Sprintf("SharedKey %s:%s", c.customerID, encodedHash)
+	return authorization, err
 }
 
-func (l *omsLogger) Close() error {
-	return nil
+func main() {
+	omsclient := NewOmsLogClient(omscustomerID, omssharedKey, omsPostTimeout)
+
+	// An example JSON data message to post
+	msg := &omsMessage{
+			SourceSystem:   "MySystemName",
+			ContainerID:    "1234567890",
+			ContainerName:  "mycontainer",
+			TimeGenerated:  time.Now().Format(time.RFC3339),
+			LogEntry:       "super important log event",
+		}
+
+	buffer, err := json.Marshal(msg)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	postErr := omsclient.PostData(&buffer, "ContainerLog")
+	if postErr != nil {
+		fmt.Println("error:", postErr)
+	}
 }
